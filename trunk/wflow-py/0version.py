94a09ad654c40445972d502882624fba276bb341
jaapschellekens@gmail.com <jaapschellekens@gmail.com@9507bdaf-6f5e-49e7-af1b-cbd99a6d813d>
2014-03-18 08:00:11 +0000

    Make bettter exe with freezer including arch

--- trunk/wflow-py/0version.py
+++
@@ -1,2 +0,0 @@
-VERSION="0.93 " + "$Id: 0version.py 882 2013-12-10 13:00:37Z schelle $"
-WFVERSION="0.93"
---
+++ trunk/wflow-py/_version.py
@@ -0,0 +1,2 @@
+VERSION="9:10M $Id: mkversion.py 550 2012-11-27 19:17:46Z schelle $"
+MVERSION="1.0RC2"
