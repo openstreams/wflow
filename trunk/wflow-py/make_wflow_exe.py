d3348cd0cb856ee6e86d566e368c228d80c12b24
jaapschellekens <jaapschellekens@gmail.com>
2014-04-10 10:35:06 +0000

    made tag: 1.0RC2

---
+++ trunk/wflow-py/make_wflow_exe.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from bbfreeze import Freezer
+from _version import *
+import ctypes
+
+nrbits = str(ctypes.sizeof(ctypes.c_voidp) * 8)
+
+
+
+f = Freezer("Wflow"+MVERSION+'-'+nrbits)
+f.addScript("wflow/__init__.py")
+f.addScript("wflow/wflow_sbm.py")
+f.addScript("wflow/wflow_hbv.py")
+f.addScript("wflow/wflow_adapt.py")
+f.addScript("wflow/wflow_delwaq.py")
+f.addScript("wflow/wflow_wave.py")
+f.addScript("wflow/wflow_gr4.py")
+f.addScript("wflow/wflow_floodmap.py")
+#f.addScript("wflow/plottss.py")
+f.addScript("Scripts/wflow_prepare_step1.py")
+f.addScript("Scripts/wflow_prepare_step2.py")
+#f.addScript("wflow/wflow_fit.py") # Does not work becuse of QT
+f()    # starts the freezing process
