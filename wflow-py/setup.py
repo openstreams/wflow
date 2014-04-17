af44f8f40198557b3bd5c324342c1372dfa52225
jaapschellekens <jaapschellekens@gmail.com>
2014-04-17 19:58:32 +0000

    Added tss2xml extension to framework

--- wflow-py/setup.py
+++ wflow-py/setup.py
@@ -10,7 +10,7 @@ setup(name='wflow',
       author_email='jaap.schellekens@deltares.nl',
       url='http://www.openstreams.nl',
       license = "GPL",
-      scripts=['wflow/wflow_extract.py','wflow/wflow_sceleton.py','wflow/wflow_gr4.py','wflow/plottss.py','wflow/wflow_wave.py','wflow/wflow_cqf.py','wflow/wflow_floodmap.py','wflow/wflow_upscale.py','wflow/wflow_fit.py','wflow/wflow_adapt.py','wflow/wflow_delwaq.py','Scripts/wflow_prepare_step1.py','Scripts/wflow_prepare_step2.py','wflow/wflow_sbm.py','wflow/wflow_hbv.py','wflow/wflow_upscale.py'],
+      scripts=['Scripts/tss2xml.py','wflow/wflow_extract.py','wflow/wflow_sceleton.py','wflow/wflow_gr4.py','wflow/plottss.py','wflow/wflow_wave.py','wflow/wflow_cqf.py','wflow/wflow_floodmap.py','wflow/wflow_upscale.py','wflow/wflow_fit.py','wflow/wflow_adapt.py','wflow/wflow_delwaq.py','Scripts/wflow_prepare_step1.py','Scripts/wflow_prepare_step2.py','wflow/wflow_sbm.py','wflow/wflow_hbv.py','wflow/wflow_upscale.py'],
       description='the wflow hydrological models (part of OpenStreams)',
       )
 
