f5a53fb5f11adef8ef2e958b6d7ac9ecf6b44456
jaapschellekens <jaapschellekens@gmail.com>
2014-05-13 12:20:29 +0000

    updated version

--- wflow-py/_version.py
+++ wflow-py/_version.py
@@ -1,2 +1,2 @@
-VERSION="31:39M $Id: mkversion.py 550 2012-11-27 19:17:46Z schelle $"
-MVERSION="1.0-RC3-dev:31:39M"
+VERSION="31-45M $Id: mkversion.py 550 2012-11-27 19:17:46Z schelle $"
+MVERSION="1.0-RC3-dev-31-45M"
