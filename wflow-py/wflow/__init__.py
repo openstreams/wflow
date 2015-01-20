528bb2cacf4ab3b10799a8b1cc47ca151a2ad986
jaapschellekens <jaapschellekens@gmail.com>
2015-01-20 12:13:07 +0000

    Documentation update

--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework","stats"]
 __version__="1.0-fin"
-__release__="1.0-RC7-fin-190-201M"
+__release__="1.0-RC8-fin-203-210"
 import osgeo.gdal as gdal
