ba069312be282e584731f4e7fb3d2ee1277e497e
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2015-06-25 15:26:24 +0200

    x

--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -2,5 +2,7 @@ __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework"
 __version__="1.0-fin"
 __release__="1.0-RC8-fin-203-210"
 
+import sys
+
 if not 'sphinx' in sys.modules:
 	import osgeo.gdal as gdal
