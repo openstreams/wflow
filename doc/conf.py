d7ef3e09b502c1442a9775d5fb04df53eda24fbb
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2015-06-26 11:30:11 +0200

    conf

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
  
 MOCK_MODULES = ['osgeo.gdal','osgeo.gdalconst',
                 'osgeo','netCDF4',
-                '_pcraster','pcraster','pcraster.framework']
+                '_pcraster','pcraster','pcraster.framework','netCDF4_utils']
 
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
