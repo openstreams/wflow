e97f58f9c958bd68869371707e180b394c163832
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2015-07-13 10:06:25 +0200

    update to also get non-raster values via bmi

--- wflow-py/Scripts/pcr2netcdf.py
+++ wflow-py/Scripts/pcr2netcdf.py
@@ -197,6 +197,7 @@ def write_netcdf_timeseries(srcFolder, srcPrefix, trgFile, trgVar, trgUnits, trg
     - maxbuf = 600: number of timesteps to buffer before writing
     
     """
+    complevel=9
 
     # if necessary, make trgPrefix maximum of 8 characters    
     if len(srcPrefix) > 8:
@@ -214,7 +215,7 @@ def write_netcdf_timeseries(srcFolder, srcPrefix, trgFile, trgVar, trgUnits, trg
         nc_var = nc_trg.variables[trgVar]
     except:
         # prepare the variable
-        nc_var = nc_trg.createVariable(trgVar, 'f4', ('time', 'lat', 'lon',), fill_value=-9999., zlib=zlib,least_significant_digit=least_significant_digit)
+        nc_var = nc_trg.createVariable(trgVar, 'f4', ('time', 'lat', 'lon',), fill_value=-9999., zlib=zlib,least_significant_digit=least_significant_digit,complevel=complevel)
         nc_var.units = trgUnits
         nc_var.standard_name = trgName
         print metadata
