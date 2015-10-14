4266c041b8a47855981a1489b4aa031f41046323
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2015-10-14 21:03:16 +0200

    small fixes

--- wflow-py/wflow/wflow_hbv.py
+++ wflow-py/wflow/wflow_hbv.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#TODO: remove dots in dynamic phase (default pcraster progress (how?)
-#TODO: formal test runs against SMHI model
 #TODO: split off routing
 
 """
