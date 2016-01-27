facc3a7f729f6910dac37d4761992c70d238fcbd
tanjaeuser <tanjaeuser@gmail.com>
2016-01-27 16:10:56 +0100

    snow and agriculture reservoir added

--- examples/batches/run_topoflex.bat
+++ examples/batches/run_topoflex.bat
@@ -1,3 +1,3 @@
 cd
-python ../../wflow-py/Sandbox/wflow_topoflex/wflow_topoflex.py -C ../wflow_orientale_topoflex -R test_run_reservoirs_names -c wflow_orientale_topoflex_zero_res.ini -I -T 20
+python ../../wflow-py/wflow/wflow_topoflex.py -C ../wflow_orientale_topoflex -R test_run_reservoirs_names -c wflow_orientale_topoflex_zero_res.ini -I -T 20
 pause
