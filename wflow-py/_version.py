a5aa8cc8ba206278f13055cfb8316b754e6c0382
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-04-06 07:05:53 +0200

    netcdf bug

--- wflow-py/_version.py
+++ wflow-py/_version.py
@@ -1,2 +1,2 @@
-VERSION="1.0.master.1"
-MVERSION="1.0.master"
+VERSION="1.0.2016.02-afghanistan.1"
+MVERSION="1.0.2016.02-afghanistan"
--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework","stats"]
-__version__="1.0.master"
-__release__="1.0.master.1"
+__version__="1.0.2016.02-afghanistan"
+__release__="1.0.2016.02-afghanistan.1"
 import osgeo.gdal as gdal
