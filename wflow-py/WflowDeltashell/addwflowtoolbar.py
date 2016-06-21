08a987702d1e404792fecefc8fbc97b005449a20
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-06-21 20:20:37 +0200

    add deltashell scripts
    Former-commit-id: eedb5220b1f41ddd4a1883190621890ddfc105c7

--- wflow-py/WflowDeltashell/addwflowtoolbar.py
+++ wflow-py/WflowDeltashell/addwflowtoolbar.py
@@ -1,21 +1,39 @@
-from Scripts.Shortcuts import *
+from WflowDeltashell.Shortcuts import *
+from WflowDeltashell.plotcsv import *
+
 
 def OpenDoc(url):
-	import webbrowser
-	webbrowser.open(url)
+	import Libraries.StandardFunctions as sf
+	from DelftTools.Utils import Url
+	murl = Url(url,url)
+	sf.OpenView(murl)
 
+def notimplemented():
+	print "Not implemented yet..."
 
 name = "Web Documentation"
 tabName = "Wflow-Tools"
-groupName = "Wflow"
+groupName = "Internet"
 
 CreateShortcutButton(name,groupName,tabName, lambda: OpenDoc("http://wflow.readthedocs.io/en/latest/"), None)
 
 name = "Github"
 tabName = "Wflow-Tools"
-groupName = "Wflow"
+groupName = "Internet"
 
 CreateShortcutButton(name,groupName,tabName, lambda: OpenDoc("http://github.com/openstreams/wflow"), None)
+
+name = "Plotcsv"
+tabName = "Wflow-Tools"
+groupName = "Plots"
+
+CreateShortcutButton(name,groupName,tabName, lambda: plotit(getcsvname()), None)
+
+name = "Netcdf Input"
+tabName = "Wflow-Tools"
+groupName = "Conversion"
+
+CreateShortcutButton(name,groupName,tabName, lambda: notimplemented(), None)
 
 #RemoveShortcut(name,groupName,tabName)
 #RemoveShortcutsTab(tabName)
