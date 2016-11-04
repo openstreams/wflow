f1d6fe3d404d7fceb1944b7235a62fac284ec141
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-11-04 10:44:47 +0100

    cx_freeze

--- wflow-py/mkversion.py
+++ wflow-py/mkversion.py
@@ -1,25 +1,33 @@
 import os
+import datetime
 
 import subprocess
 branch = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).strip()
 
 vers='1'
+nrversion = '1.0'
 
 ###################################
-manualversion = "1.0" + "." + branch + "." + vers
-manualmainversion = "1.0" + "." + branch
+manualversion = nrversion + "." + branch + "." + vers
+manualmainversion = nrversion + "." + branch
+version = nrversion = '1.0' + "." + vers
 ###################################
 a = open("_version.py","w")
 
+build=datetime.datetime.now()
+
 a.write("VERSION=\"" + manualversion +  "\"\n")
 a.write("MVERSION=\"" + manualmainversion +"\"\n")
-
+a.write("NVERSION=\"" + version +"\"\n")
+a.write("BUILD=\"" + str(build) +"\"\n")
 a.close()
 
 a = open("wflow/__init__.py","w")
 a.write("__all__ = [\"wflow_funcs\",\"wflow_adapt\",\"wflow_lib\",\"pcrut\",\"wf_DynamicFramework\",\"stats\"]\n")
 a.write("__version__=\"" + manualmainversion + "\"\n")
 a.write("__release__=\"" + manualversion + "\"\n")
+a.write("__versionnr__=\"" + version + "\"\n")
+a.write("__build__=\"" + str(build) + "\"\n")
 a.write("import osgeo.gdal as gdal")
 
 
--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -1,4 +1,6 @@
 __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework","stats"]
 __version__="1.0.2016.03"
 __release__="1.0.2016.03.1"
+__versionnr__="1.0.1"
+__build__="2016-11-04 10:44:17.678000"
 import osgeo.gdal as gdal
