f86d3deb279b6058060e2762cfe64899ef322e10
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-11-04 10:49:52 +0100

    cx_freeze

--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -2,5 +2,5 @@ __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework"
 __version__="1.0.2016.03"
 __release__="1.0.2016.03.1"
 __versionnr__="1.0.1"
-__build__="2016-11-04 10:44:17.678000"
+__build__="2016-11-04 10:48:33.994000"
 import osgeo.gdal as gdal
