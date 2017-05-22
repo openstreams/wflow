460a30b28bd01f0bf7ee267a704b54ab2c920ca8
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2017-05-22 11:09:04 +0200

    added routing

--- wflow-py/make_wflow_exe_cx.py
+++ wflow-py/make_wflow_exe_cx.py
@@ -121,6 +121,8 @@ if sys.platform == 'win32':
     data_files.extend(mkdatatuples(MKL_files,destdir="."))
     # pcraster dll's
     ddir = "c:/pcraster/lib/"
+    # for teamcity
+    ddir = "D:/BuildAgent/work/WFlow_BuildExe/pcraster-4.1.0_x86-64"
     data_files.extend(mkdatatuples(glob.glob(ddir + "/*.dll"),destdir='.'))
 
 # GDAL data files
