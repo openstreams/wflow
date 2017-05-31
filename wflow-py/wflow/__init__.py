412ef230eb57e0be0f5393b3f2fe8efca74d9d4f
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2017-05-31 14:12:57 +0200

    build stuff

--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -3,13 +3,19 @@ __version__="1.0.master"
 __release__="1.0.master.1"
 __versionnr__="1.0.1"
 __build__="2017-05-23 08:34:15.293000"
+
+
 import osgeo.gdal as gdal
 
 import os, sys
 if hasattr(sys, "frozen"):
+    print("wflow: frozen")
     _ROOT = os.path.abspath(os.path.dirname(__file__)).split("library.zip")[0]
     os.environ['GDAL_DATA'] = os.path.join(_ROOT,'gdal-data')
+    _ROOT = os.path.abspath(os.path.dirname(__file__)).split("library.zip")[0]
+    os.environ['PATH'] = os.environ['PATH'] + ";" + os.path.join(_ROOT)
 else:
+    print("wflow: python")
     _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 def get_data(path):
