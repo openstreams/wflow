1cdd62a662d773814de95ff9b220d4c6532339c5
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2017-06-01 15:38:23 +0200

    build stuff

--- wflow-py/wflow/__init__.py
+++ wflow-py/wflow/__init__.py
@@ -2,21 +2,21 @@ __all__ = ["wflow_funcs","wflow_adapt","wflow_lib","pcrut","wf_DynamicFramework"
 __version__="1.0.master"
 __release__="1.0.master.1"
 __versionnr__="1.0.1"
-__build__="2017-05-23 08:34:15.293000"
-
-
-import osgeo.gdal as gdal
-
+__build__="2017-06-01 15:37:12.049000"
 import os, sys
 if hasattr(sys, "frozen"):
-    print("wflow: frozen")
+    print('Frozen...')
     _ROOT = os.path.abspath(os.path.dirname(__file__)).split("library.zip")[0]
     os.environ['GDAL_DATA'] = os.path.join(_ROOT,'gdal-data')
-    _ROOT = os.path.abspath(os.path.dirname(__file__)).split("library.zip")[0]
-    os.environ['PATH'] = os.environ['PATH'] + ";" + os.path.join(_ROOT)
+    os.environ['PATH'] = _ROOT + ';' + os.environ['PATH']
+    os.environ['PYTHONPATH'] = _ROOT + ';' + os.environ['PYTHONPATH']
+    sys.path.insert(0,_ROOT)
+    if _ROOT not in os.environ['PATH']:
+        print('Root dir of binary not insystem path. This may cause problems...')
 else:
-    print("wflow: python")
     _ROOT = os.path.abspath(os.path.dirname(__file__))
+
+import osgeo.gdal as gdal
 
 def get_data(path):
     return os.path.join(_ROOT, 'data', path)
