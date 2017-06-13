2f1942d7b14dd5dcea85e74c126c2299b80e0579
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2017-06-13 13:35:26 +0200

    adapter changes working to zero config: remove fewsrun

--- wflow-py/_version.py
+++ wflow-py/_version.py
@@ -1,4 +1,4 @@
 # This file is made by mkversion.py, do not edit!VERSION="1.0.master.1"
 MVERSION="1.0.master"
 NVERSION="1.0.1"
-BUILD="2017-06-13 10:10:58.703000"
+BUILD="2017-06-13 13:19:20.351000"
