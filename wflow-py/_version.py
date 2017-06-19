4fcd2e520e5dfe89de46075893c3d4a98e030ba0
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2017-06-19 15:26:32 +0200

    start time shit for bmirunner

--- wflow-py/_version.py
+++ wflow-py/_version.py
@@ -1,4 +1,4 @@
 # This file is made by mkversion.py, do not edit!VERSION="1.0.timestuff-rework.1"
 MVERSION="1.0.timestuff-rework"
 NVERSION="1.0.1"
-BUILD="2017-06-19 13:48:52.564000"
+BUILD="2017-06-19 15:17:10.906000"
