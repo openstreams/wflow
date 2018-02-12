a32f92ead36f9ed89d489b5d64fe27f344fbcb37
Martijn Visser <mgvisser@gmail.com>
2018-02-12 14:29:43 +0100

    adapt wflow_adapt to new log format, cleanup
    Many of the files processed were not closed.

--- wflow-py/wflow/wflow_adapt.py
+++ wflow-py/wflow/wflow_adapt.py
@@ -63,8 +63,9 @@ from glob import *
 import logging
 import logging.handlers
 import ConfigParser
+import numpy
 
-import  wflow.wflow_lib as wflow_lib
+import wflow.wflow_lib as wflow_lib
 import wflow.pcrut as pcrut
 
  
@@ -140,19 +141,19 @@ def log2xml(logfile,xmldiag):
 
     trans = {'WARNING': '2', 'ERROR': '1', 'INFO': '3','DEBUG': '4'}
     if os.path.exists(logfile):
-        ifile = open(logfile,"r")
-        ofile = open(xmldiag,"w")
-        all = ifile.readlines()
+        with open(logfile, "r") as fi:
+            lines = fi.readlines()
 
-        ofile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-        ofile.write("<Diag xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n")
-        ofile.write("xmlns=\"http://www.wldelft.nl/fews/PI\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI \n")
-        ofile.write("http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_diag.xsd\" version=\"1.2\">\n")
-        for aline in all:
-            alineesc = aline.translate(None,"><&\"\'")
-            lineparts = alineesc.strip().split(" - ")
-            ofile.write("<line level=\"" + trans[lineparts[2]] + "\" description=\"" + lineparts[3] + " [" + lineparts[0] + "]\"/>\n")
-        ofile.write("</Diag>\n")
+        with open(xmldiag, "w") as fo:
+            fo.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            fo.write("<Diag xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n")
+            fo.write("xmlns=\"http://www.wldelft.nl/fews/PI\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI \n")
+            fo.write("http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_diag.xsd\" version=\"1.2\">\n")
+            for aline in lines:
+                alineesc = aline.translate(None,"><&\"\'")
+                lineparts = alineesc.strip().split(" - ")
+                fo.write("<line level=\"" + trans[lineparts[3]] + "\" description=\"" + lineparts[4] + " [" + lineparts[0] + "]\"/>\n")
+            fo.write("</Diag>\n")
 
 
 def pixml_state_updateTime(inxml,outxml,DT):
@@ -171,22 +172,22 @@ def pixml_state_updateTime(inxml,outxml,DT):
     """
 
     if os.path.exists(inxml):
-        file = open(inxml, "r")
-        all = file.readlines()
-        ofile = open(outxml,'w')
         datestr = DT.strftime('%Y-%m-%d')
         timestr = DT.strftime('%H:%M:%S')
-        
-        for aline in all:
-            pos = aline.find('dateTime')
-            if pos >= 0:
-                ofile.write("<dateTime date=\"" + datestr + "\" time=\"" + timestr + "\"/>\n")
-            else:
-                ofile.write(aline)
 
-        ofile.close()
+        with open(inxml, 'r') as fi:
+            lines = fi.readlines()
+
+        with open(outxml, 'w') as fo:
+            for aline in lines:
+                pos = aline.find('dateTime')
+                if pos >= 0:
+                    fo.write("<dateTime date=\"" + datestr + "\" time=\"" + timestr + "\"/>\n")
+                else:
+                    fo.write(aline)
+
     else:
-        print inxml + " does not exists."
+        print(inxml + " does not exists.")
         
 
         
@@ -201,30 +202,31 @@ def pixml_totss_dates (nname,outputdir):
     """
     
     if os.path.exists(nname):
-        file = open(nname, "r")
-        tree = parse(file)
+        with open(nname, 'r') as f:
+            tree = parse(f)
+
         PItimeSeries = tree.getroot()
         series = PItimeSeries.findall('.//{' + fewsNamespace + '}series')
         
         events = series[0].findall('.//{' + fewsNamespace + '}event') 
-        f = open(outputdir + '/YearDay.tss','w')
-        ff = open(outputdir + '/Hour.tss','w')
-        # write the header
-        f.write('Parameter YearDay taken from ' + nname + '\n')
-        ff.write('Parameter Hour taken from ' + nname + '\n')
-        f.write('2\n')
-        ff.write('2\n')
-        for i in range(1,3):
-            f.write('Data column ' + str(i) + '\n')
-            ff.write('Data column ' + str(i) + '\n')
-        i=1        
-        for ev in events:        
-            dt = datetime.strptime(ev.attrib['date'] + ev.attrib['time'],'%Y-%m-%d%H:%M:%S')
-            f.write(str(i) +'\t' + dt.strftime('%j\n'))
-            ff.write(str(i) +'\t' + dt.strftime('%H\n'))
-            i = i+1    
+        with open(outputdir + '/YearDay.tss','w') as f:
+            with open(outputdir + '/Hour.tss','w') as ff:
+                # write the header
+                f.write('Parameter YearDay taken from ' + nname + '\n')
+                ff.write('Parameter Hour taken from ' + nname + '\n')
+                f.write('2\n')
+                ff.write('2\n')
+                for i in range(1,3):
+                    f.write('Data column ' + str(i) + '\n')
+                    ff.write('Data column ' + str(i) + '\n')
+                i = 1        
+                for ev in events:        
+                    dt = datetime.strptime(ev.attrib['date'] + ev.attrib['time'],'%Y-%m-%d%H:%M:%S')
+                    f.write(str(i) +'\t' + dt.strftime('%j\n'))
+                    ff.write(str(i) +'\t' + dt.strftime('%H\n'))
+                    i += 1
     else:
-        print nname + " does not exists."
+        print(nname + " does not exists.")
     
 
 def pixml_totss(nname,outputdir):
@@ -241,8 +243,9 @@ def pixml_totss(nname,outputdir):
     """
     
     if os.path.exists(nname):
-        file = open(nname, "r")
-        tree = parse(file)
+        with open(nname, 'r') as f:
+            tree = parse(f)
+        
         PItimeSeries = tree.getroot()
         seriesStationList=PItimeSeries.findall('.//{' + fewsNamespace + '}stationName')
         LocList=[]
@@ -273,7 +276,7 @@ def pixml_totss(nname,outputdir):
             events = thisS.findall('.//{' + fewsNamespace + '}event') 
             locs = thisS.findall('.//{' + fewsNamespace + '}locationId')
             
-            i=0;
+            i = 0
             for ev in events:
                 parlocs[par] = 1
                 if val.has_key((i,par)):
@@ -282,23 +285,23 @@ def pixml_totss(nname,outputdir):
                     parlocs[par] = parlocs[par] + 1            
                 else:        
                     val[i,par] = ev.attrib['value']
-                i = i+1
+                i += 1
         nrevents = i
         
 
         for par in uniqueParList:
-            f = open(outputdir + '/' + par + '.tss','w')
-            # write the header
-            f.write('Parameter ' + par + ' taken from ' + nname + '\n')
-            f.write(str(colsinfile + 1) + '\n')
-            f.write('Timestep\n')
-            for i in range(0,colsinfile):
-                f.write('Data column ' + str(i) + '\n')
-            for i in range(0,nrevents):
-                f.write(str(i+1) + '\t' + val[i,par] + '\n')
+            with open(outputdir + '/' + par + '.tss','w') as f:
+                # write the header
+                f.write('Parameter ' + par + ' taken from ' + nname + '\n')
+                f.write(str(colsinfile + 1) + '\n')
+                f.write('Timestep\n')
+                for i in range(0,colsinfile):
+                    f.write('Data column ' + str(i) + '\n')
+                for i in range(0,nrevents):
+                    f.write(str(i+1) + '\t' + val[i,par] + '\n')
                         
     else:
-        print nname + " does not exists."
+        print(nname + " does not exists.")
 
 
 
@@ -322,14 +325,14 @@ def tss_topixml(tssfile,xmlfile,locationname,parametername,Sdate,timestep):
     if len(tss.shape) > 1:
         dumm = tss[0,:].copy()
         dumm[:] = -999.0
-        tss =  pcrut.numpy.vstack((dumm,tss))
+        tss =  numpy.vstack((dumm,tss))
     else:
         dumm = tss.copy()
         dumm[:] = -999.0
-        tss =  pcrut.numpy.vstack((dumm,tss))
+        tss =  numpy.vstack((dumm,tss))
   
     # replace NaN with missing values
-    tss[pcrut.numpy.isnan(tss)] = missval
+    tss[numpy.isnan(tss)] = missval
 
     trange = timedelta(seconds=timestep * (tss.shape[0]))
 
@@ -345,42 +348,42 @@ def tss_topixml(tssfile,xmlfile,locationname,parametername,Sdate,timestep):
 
     Edatestr = Edate.strftime('%Y-%m-%d')
     Etimestr = Edate.strftime('%H:%M:%S')
-    ofile = open(xmlfile,'w')
-    ofile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    ofile.write("<TimeSeries xmlns=\"http://www.wldelft.nl/fews/PI\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_timeseries.xsd\" version=\"1.2\">\n")
-    ofile.write("<timeZone>0.0</timeZone>\n")
-    count = 0
+    with open(xmlfile, 'w') as fo:
+        fo.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        fo.write("<TimeSeries xmlns=\"http://www.wldelft.nl/fews/PI\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_timeseries.xsd\" version=\"1.2\">\n")
+        fo.write("<timeZone>0.0</timeZone>\n")
+        count = 0
 
-    for col in tss.transpose():
-        count = count + 1
-        ofile.write("<series>\n")
-        ofile.write("<header>\n")
-        ofile.write("<type>instantaneous</type>\n")
-        ofile.write("<locationId>" + header[count-1] + "</locationId>\n")
-        ofile.write("<parameterId>" + parametername + "</parameterId>\n")
-        ofile.write("<timeStep unit=\"second\" multiplier=\"" + str(timestep) + "\"/>\n")
-        ofile.write("<startDate date=\"" + Sdatestr +"\" time=\""+ Stimestr + "\"/>\n")
-        ofile.write("<endDate date=\"" + Edatestr + "\" time=\"" + Etimestr + "\"/>\n")
-        ofile.write("<missVal>"+str(missval)+"</missVal>\n")
-        ofile.write("<stationName>" + header[count-1] +  "</stationName>\n")
-        ofile.write("</header>\n")
-        # add data here
-        xdate = Sdate
-        xcount = 1
-        for pt in col:
-            if xcount > 1:
-                Ndatestr = xdate.strftime('%Y-%m-%d')
-                Ntimestr = xdate.strftime('%H:%M:%S')
-                ofile.write("<event date=\"" + Ndatestr + "\" time=\"" + Ntimestr + "\" value=\"" + str(pt) + "\" />\n")
-                xdate = xdate + timedelta(seconds=timestep)
-            xcount = xcount + 1
-        ofile.write("</series>\n")
-        
-    ofile.write("</TimeSeries>\n")
-    ofile.close()
-                
+        for col in tss.transpose():
+            count = count + 1
+            fo.write("<series>\n")
+            fo.write("<header>\n")
+            fo.write("<type>instantaneous</type>\n")
+            fo.write("<locationId>" + header[count-1] + "</locationId>\n")
+            fo.write("<parameterId>" + parametername + "</parameterId>\n")
+            fo.write("<timeStep unit=\"second\" multiplier=\"" + str(timestep) + "\"/>\n")
+            fo.write("<startDate date=\"" + Sdatestr +"\" time=\""+ Stimestr + "\"/>\n")
+            fo.write("<endDate date=\"" + Edatestr + "\" time=\"" + Etimestr + "\"/>\n")
+            fo.write("<missVal>"+str(missval)+"</missVal>\n")
+            fo.write("<stationName>" + header[count-1] +  "</stationName>\n")
+            fo.write("</header>\n")
+            # add data here
+            xdate = Sdate
+            xcount = 1
+            for pt in col:
+                if xcount > 1:
+                    Ndatestr = xdate.strftime('%Y-%m-%d')
+                    Ntimestr = xdate.strftime('%H:%M:%S')
+                    fo.write("<event date=\"" + Ndatestr + "\" time=\"" + Ntimestr + "\" value=\"" + str(pt) + "\" />\n")
+                    xdate = xdate + timedelta(seconds=timestep)
+                xcount = xcount + 1
+            fo.write("</series>\n")
+
+        fo.write("</TimeSeries>\n")
+
     return tss
-    
+
+
 def mapstackxml(mapstackxml,mapstackname,locationname,parametername,Sdate,Edate,timestepsecs):
     """
         writes a mapstack xml file
@@ -389,31 +392,32 @@ def mapstackxml(mapstackxml,mapstackname,locationname,parametername,Sdate,Edate,
     Stimestr = Sdate.strftime('%H:%M:%S')
     Edatestr = Edate.strftime('%Y-%m-%d')
     Etimestr = Edate.strftime('%H:%M:%S')
-    ofile = open(mapstackxml,'w')
-    ofile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    ofile.write("<MapStacks xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.wldelft.nl/fews/PI\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_mapstacks.xsd\" version=\"1.2\">\n")
-    ofile.write("<geoDatum>WGS 1984</geoDatum>\n")
-    ofile.write("<timeZone>0.0</timeZone>\n")
-    ofile.write("<mapStack>\n")
-    ofile.write("<locationId>"+locationname+"</locationId>\n")
-    ofile.write("<parameterId>"+parametername+"</parameterId>\n")
-    ofile.write("<timeStep unit=\"second\" multiplier=\"" + str(timestepsecs) + "\"/>\n")
-    ofile.write("<startDate date=\""+Sdatestr+"\" time=\""+Stimestr+"\"/>\n")
-    ofile.write("<endDate date=\""+Edatestr+"\" time=\""+Etimestr+"\"/>\n")
-    ofile.write("<file>\n")
-    ofile.write("    <pcrgrid file=\""+mapstackname+"\"/>\n")
-    ofile.write("</file>\n")
-    ofile.write("</mapStack>\n")
-    ofile.write("</MapStacks>\n")
-    ofile.close()
+    with open(mapstackxml, 'w') as fo:
+        fo.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        fo.write("<MapStacks xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.wldelft.nl/fews/PI\" xsi:schemaLocation=\"http://www.wldelft.nl/fews/PI http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_mapstacks.xsd\" version=\"1.2\">\n")
+        fo.write("<geoDatum>WGS 1984</geoDatum>\n")
+        fo.write("<timeZone>0.0</timeZone>\n")
+        fo.write("<mapStack>\n")
+        fo.write("<locationId>"+locationname+"</locationId>\n")
+        fo.write("<parameterId>"+parametername+"</parameterId>\n")
+        fo.write("<timeStep unit=\"second\" multiplier=\"" + str(timestepsecs) + "\"/>\n")
+        fo.write("<startDate date=\""+Sdatestr+"\" time=\""+Stimestr+"\"/>\n")
+        fo.write("<endDate date=\""+Edatestr+"\" time=\""+Etimestr+"\"/>\n")
+        fo.write("<file>\n")
+        fo.write("    <pcrgrid file=\""+mapstackname+"\"/>\n")
+        fo.write("</file>\n")
+        fo.write("</mapStack>\n")
+        fo.write("</MapStacks>\n")
+
 
 def getTimeStepsfromRuninfo(xmlfile,timestepsecs):
     """
         Gets the number of  timesteps from the FEWS runinfo file.
     """
     if os.path.exists(xmlfile):
-        file = open(xmlfile, "r")
-        tree = parse(file)
+        with open(xmlfile, "r") as f:
+            tree = parse(f)
+
         runinf = tree.getroot()
         sdate=runinf.find('.//{' + fewsNamespace + '}startDateTime')
         ttime = sdate.attrib['time']
@@ -431,7 +435,7 @@ def getTimeStepsfromRuninfo(xmlfile,timestepsecs):
         else:
             return diff.days  + 1# Should actually be + 1 but fews starts at 0!
     else:
-        print xmlfile + " does not exists."
+        print(xmlfile + " does not exists.")
 
 
 
@@ -440,24 +444,25 @@ def getEndTimefromRuninfo(xmlfile):
     Gets the endtime of the run from the FEWS runinfo file
     """
     if os.path.exists(xmlfile):
-        file = open(xmlfile, "r")
-        tree = parse(file)
+        with open(xmlfile, "r") as f:
+            tree = parse(f)
         runinf = tree.getroot()
         edate=runinf.find('.//{' + fewsNamespace + '}endDateTime')
         ed = datetime.strptime(edate.attrib['date'] + edate.attrib['time'],'%Y-%m-%d%H:%M:%S')
     else:
-        print xmlfile + " does not exists."
+        print(xmlfile + " does not exists.")
         ed = None
         
     return ed
+
 
 def getStartTimefromRuninfo(xmlfile):
     """ 
     Gets the starttime from the FEWS runinfo file
     """
     if os.path.exists(xmlfile):
-        file = open(xmlfile, "r")
-        tree = parse(file)
+        with open(xmlfile, "r") as f:
+            tree = parse(f)
         runinf = tree.getroot()
         edate=runinf.find('.//{' + fewsNamespace + '}startDateTime')
         ttime = edate.attrib['time']
@@ -476,13 +481,13 @@ def getMapStacksFromRuninfo(xmlfile):
     """
     
     if os.path.exists(xmlfile):
-        file = open(xmlfile, "r")
-        tree = parse(file)
+        with open(xmlfile, "r") as f:
+            tree = parse(f)
         runinf = tree.getroot()
         edate=runinf.find('.//{' + fewsNamespace + '}startDateTime')
         ed = datetime.strptime(edate.attrib['date'] + edate.attrib['time'],'%Y-%m-%d%H:%M:%S')
     else:
-        print xmlfile + " does not exists."
+        print(xmlfile + " does not exists.")
         
     return ed
 
@@ -492,12 +497,12 @@ def pre_adapter(INxmlTimeSeries,logger):
         logger.info("Converting " + xmlTimeSeries + " ..... ")
         pixml_totss(xmlTimeSeries,case + '/intss/')
         pixml_totss_dates(xmlTimeSeries,case + '/intss/')
-        #writeNrTimesteps()
+        # writeNrTimesteps()
     
 def usage():
-    print "wflow_adapter -M Pre -t InputTimeseriesXml -I inifile"
-    print "wflow_adapter -M Post -t InputTimeseriesXml -s inputStateFile -I inifile"
-    print "              -o outputStateFile -r runinfofile -w workdir -C case"
+    print("wflow_adapter -M Pre -t InputTimeseriesXml -I inifile")
+    print("wflow_adapter -M Post -t InputTimeseriesXml -s inputStateFile -I inifile")
+    print("              -o outputStateFile -r runinfofile -w workdir -C case")
 
 
 
