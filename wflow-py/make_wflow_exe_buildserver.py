57477fd46de85b68ac2d57b89712a0343c65bfe4
verseve <willem.vanverseveld@gmail.com>
2018-03-14 17:25:12 +0100

    add extra numpy lib (Importing the multiarray numpy extension module failed...of exe)

--- wflow-py/make_wflow_exe_buildserver.py
+++ wflow-py/make_wflow_exe_buildserver.py
@@ -109,6 +109,7 @@ nrbits = str(ctypes.sizeof(ctypes.c_voidp) * 8)
 
 thename = "wflow-bin/Wflow"+MVERSION+'-'+target+'-'+sys.platform+'-'+nrbits
 
+
 packages = ["osgeo","numpy","numpy.core._methods"]
 
 if target == 'openda':
