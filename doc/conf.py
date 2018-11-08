3f3e1a1297bab132981b7c572df5ddd80558f8d0
Martijn Visser <mgvisser@gmail.com>
2018-11-08 10:34:26 +0100

    pngmath is deprecated
    try mathjax, if not then imgmath is a more direct replacement

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ import wflow
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo','sphinx.ext.pngmath' ,
+extensions = ['sphinx.ext.todo','sphinx.ext.mathjax' ,
            'matplotlib.sphinxext.only_directives',
            'matplotlib.sphinxext.plot_directive',
            'sphinx.ext.autodoc',
