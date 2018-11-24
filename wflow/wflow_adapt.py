d5cfa3d334291d0fa05ed5c98115fc3b8b6b5989
verseve <willem.vanverseveld@gmail.com>
2018-11-24 19:05:25 +0100

    translate() has changed in python 3

--- wflow/wflow_adapt.py
+++ wflow/wflow_adapt.py
@@ -153,17 +153,9 @@ def log2xml(logfile, xmldiag):
                 'http://fews.wldelft.nl/schemas/version1.0/pi-schemas/pi_diag.xsd" version="1.2">\n'
             )
             for aline in lines:
-                alineesc = aline.translate(None, "><&\"'")
-                lineparts = alineesc.strip().split(" - ")
-                fo.write(
-                    '<line level="'
-                    + trans[lineparts[3]]
-                    + '" description="'
-                    + lineparts[4]
-                    + " ["
-                    + lineparts[0]
-                    + ']"/>\n'
-                )
+                translator = aline.maketrans('', '',"><&\"'")
+                alineesc = aline.translate(translator)
+                fo.write(alineesc)
             fo.write("</Diag>\n")
 
 
