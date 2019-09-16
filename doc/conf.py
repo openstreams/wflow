d49d0ec111cc2083a38d400efbac5fccfb6c0f51
verseve <willem.vanverseveld@gmail.com>
2019-09-16 11:51:23 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -56,13 +56,10 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-print(os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("pcraster-for-doc-only/python"))
 sys.path.insert(0, os.path.abspath("../wflow"))
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../Scripts"))
-
-print(sys.path)
 
 import wflow
 
