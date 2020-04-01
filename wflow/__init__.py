f3b18d6466b93aa42d73b59d1d226ec3911777d0
Martijn Visser <mgvisser@gmail.com>
2020-04-02 00:13:47 +0200

    avoid proj and matplotlib warnings with pyinstaller

--- wflow/__init__.py
+++ wflow/__init__.py
@@ -12,8 +12,6 @@ import pkg_resources
 
 # import pyproj and gdal, to be able to set paths to shared resources
 # in case we are using a frozen bundle created with pyinstaller
-import pyproj
-import pyproj.datadir
 import osgeo.gdal as gdal
 
 gdal.UseExceptions()
@@ -26,6 +24,11 @@ if getattr(sys, "frozen", False):
     if not basedir:
         basedir = os.path.dirname(sys.executable)
 
+    # filter out matplotlib deprecation warning triggered by PyInstaller
+    # https://stackoverflow.com/q/57517371
+    import warnings
+    warnings.filterwarnings("ignore", "(?s).*MATPLOTLIBDATA.*", category=UserWarning)
+
     # use the included gdal-data
     gdal_data_path = os.path.join(basedir, "gdal-data")
     gdal.SetConfigOption("GDAL_DATA", gdal_data_path)
@@ -33,9 +36,14 @@ if getattr(sys, "frozen", False):
 
     # set environment variable next to pyproj_datadir such
     # that child processes will inherit it
+    # first set the environment variable, to avoid a warning on pyproj import
     proj_data_dir = os.path.join(basedir, "proj-data")
-    pyproj.datadir.set_data_dir(proj_data_dir)
     os.environ["PROJ_LIB"] = proj_data_dir
+    import pyproj.datadir
+    pyproj.datadir.set_data_dir(proj_data_dir)
+
+import pyproj
+import pyproj.datadir
 
 try:
     from wflow.version import version
